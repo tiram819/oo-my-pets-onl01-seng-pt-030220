522564c88d73f64015d39222395af0f1d2e30be7
Yeasha Hassan <tiram819@gmail.com>
2020-04-04 16:52:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -68,10 +68,11 @@ class Owner
  def sell_pets
    Dog.all.each do |dog| 
       dog.mood = "nervous"
+      dog.owner = "nil"
     end
     Cat.all.each do |cat| 
       cat.mood = "nervous"
-    end
+      cat.owner = "nil"
 end
 end
 
