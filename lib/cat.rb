d2a04990e582f5d759a9c5c33b2d6518ccd4b6a0
Yeasha Hassan <tiram819@gmail.com>
2020-04-04 14:26:24 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,29 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood, :pets
+  attr_reader :name
+  
+  @@all = []
+  @@pets = {:pets => [], :cats => []}
+  
+  
+  def initialize(name, mood)
+      @name = name  #can have a name
+      @mood = "nervous" 
+      @owner = Owner
+      @@all << self
+    end 
+
+  
+    def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length 
+  end 
+	
+  
+  
+  
 end
+        
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,40 @@
-class Owner
-  # code goes here
+class Owner 
+  attr_accessor :name, :pets, :dog, :cat
+  attr_reader :species
+  
+  @@all = []
+  @@pets = {:pets => [], :dogs => [], :cats => []}
+    
+    def initialize(name)
+      @name = name  #can have a name
+      @species = "human"  #initializes with species set to human
+      @@all << self
+    end 
+  
+    
+    def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length 
+  end 
+  
+ 
+  def say_species
+    "I am a #{self.species}."
+  end
+  
+   def self.reset_all
+    @@all.clear
+  end
+  
+ 
 end
+
+
+
+      
+
+  
+   
